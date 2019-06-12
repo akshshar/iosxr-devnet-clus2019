efbbc9735092ad1d49f5bc1b2e7b99108b4dfa0b
akshshar <akshat31187@gmail.com>
2019-06-12 10:34:33 -0700

    Fixed ansible playbooks and launch scripts for openr

--- ansible/openr/launch_openr_r2.sh
+++ ansible/openr/launch_openr_r2.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -itd --restart=always --name openr --cap-add=SYS_ADMIN --cap-add=NET_ADMIN  -v /var/run/netns:/var/run/netns -v /misc/app_host:/root -v /misc/app_host/hosts_r2:/etc/hosts --hostname r2 192.168.122.11:5000/openr-xr /root/run_openr_r2.sh
+docker run -itd --restart=always --name openr --cap-add=SYS_ADMIN --cap-add=NET_ADMIN  -v /var/run/netns:/var/run/netns -v /misc/app_host:/root -v /misc/app_host/hosts_r2:/etc/hosts --hostname r2 192.168.122.11:5000/openr-xr /bin/bash -c "/root/       run_openr_r1.sh > /dev/null 2>&1"
